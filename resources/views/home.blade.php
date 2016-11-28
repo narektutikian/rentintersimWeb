181c0abfab86a0b911b9a134445de5149013d8e7
johannesMatevosyan <h.matevosyan@yahoo.com>
2016-11-28 16:50:52 +0400

    test 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,9 @@
 @section('content')
     <div class="layout">
         <div class="container">
-
             <div id="orders_list">
-                
                 <section class="filter_status">
-
                     <div class="orders_list_wrapper">
-
                         <div class="filter_text">Filter by status:</div>
                         <div class="filter_buttons">
                                     <span class="filter_option blue">
@@ -24,7 +20,6 @@
                             <span class="filter_option light_blue last">
                                         <span class="status waiting"></span><a href="{{url('filter-orderlist/Waiting')}}"> Waiting (2) </a>
                                     </span>
-
                             <div class="search_management_option">
                                 <form action="/" class="search_form_option">
                                     <input type="text" class="block_btn_30 search_input" value="search">
@@ -34,6 +29,7 @@
                                 <a href="{{url('/order/create')}}" class="add_new_user"><i class="icon-new_order"></i>New Order</a>
                             </div>
                         </div>
+                        
                         <div class="clear"></div>
                     </div>
                 </section>
