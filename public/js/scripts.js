3ac2341314d46838af3d9e3b732a093ede9ab668
Narek Tutikyan <narek@king.am>
2017-01-09 16:19:10 +0400

    time picker

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -215,7 +215,7 @@ $( document ).ready(function() {
                 show_meridian:false,
                 min_hour_value:0,
                 max_hour_value:23,
-                step_size_minutes:1,
+                step_size_minutes:15,
                 overflow_minutes:true,
                 increase_direction:'up',
                 disable_keyboard_mobile: true,
